48602454e1b2428d858ebc74efac503006d5ee73
sqliuchang <jssqliuchang@gmail.com>
2026-09-18 19:14:45 +0800

    remove luci-app-3ginfo-lite

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -77,7 +77,6 @@ UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
 UPDATE_PACKAGE "timecontrol" "sirpdboy/luci-app-timecontrol" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "gecoosac luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
-UPDATE_PACKAGE "luci-app-3ginfo-lite" "sqliuchang/luci-app-3ginfo-lite" "main"
 
 #更新软件包版本
 UPDATE_VERSION() {
